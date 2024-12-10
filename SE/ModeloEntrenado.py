a578ac3f840bc197c152abc5ceb0f7d4bcec3c73
Alexgaby18 <131205238+Alexgaby18@users.noreply.github.com>
2024-12-09 20:43:18 -0400

    Add files via upload

---
+++ SE/ModeloEntrenado.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+import numpy as np 
+modelo = tf.keras.models.load_model("phylum_model_mejorado.h5")
+
+# Función para realizar predicciones
+def predecir_phylum(caracteristicas):
+    caracteristicas = np.array(caracteristicas).reshape(1, -1)
+    # Realizar la predicción
+    prediccion_numerica = modelo.predict(caracteristicas)
+    # Obtener el índice de la clase con la mayor probabilidad
+    phylum_indice = np.argmax(prediccion_numerica)
+    # Mapear el índice de vuelta a la etiqueta del phylum
+    etiquetas_phylum = ["Protozoarios", "Poriferos", "Cnidarios", "Ctenoforos", "Platelmintos", "Anelidos", "Moluscos", "Artrópodos", "Cordados"]
+    phylum_predicho = etiquetas_phylum[phylum_indice]
+    return {"phylum_predicho":phylum_predicho, "prediccion_numerica": prediccion_numerica}
+
+# Probar la función de predicción con un ejemplo
+caracteristicas_ejemplo = [1, 0, 1, 0, 0, 0, 0]  # Cambiar según sea necesario
+phylum = predecir_phylum(caracteristicas_ejemplo)
+print(f'El phylum predicho es: {phylum["phylum_predicho"]}')
+print(phylum["prediccion_numerica"])
+
---
+++ SE/SistemaPhylum.py
@@ -0,0 +1,81 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+""" Características de Entrada (features)
+Podríamos considerar las siguientes características para cada organismo:
+
+Multicelular (0: No, 1: Sí)
+
+Con tejidos verdaderos (0: No, 1: Sí)
+
+Simetría (0: Asimétrica, 1: Radial, 2: Bilateral)
+
+Tipo de cavidad corporal (0: Ninguno, 1: Pseudocelomada, 2: Celomada)
+
+Con esqueleto (0: No, 1: Exoesqueleto, 2: Endoesqueleto)
+
+Complejidad de sistemas digestivos (0: Incompleto, 1: Completo)
+
+Ambiente (0: Acuático, 1: Terrestre) """
+
+# Características de entrada mejoradas
+features = [
+    (0, 0, 0, 0, 0, 0, 0),  # Protozoarios
+    (1, 0, 1, 0, 0, 0, 0),  # Poríferos
+    (1, 1, 1, 0, 0, 0, 1),  # Cnidarios
+    (1, 1, 1, 1, 0, 1, 0),  # Ctenóforos
+    (1, 1, 2, 0, 1, 0, 1),  # Platelmintos
+    (1, 1, 2, 1, 1, 1, 1),  # Anélidos
+    (1, 1, 2, 2, 1, 1, 0),  # Moluscos
+    (1, 1, 2, 2, 2, 1, 1),  # Artrópodos
+    (1, 1, 2, 2, 2, 1, 0)   # Cordados
+]
+
+
+targets = [0, 1, 2, 3, 4, 5, 6, 7, 8] 
+
+# Convertir listas a arrays de NumPy
+features = np.array(features)
+targets = np.array(targets)
+
+# modelo con más capas y neuronas
+modelo = tf.keras.Sequential([
+    tf.keras.layers.Dense(units=16, input_shape=[7], activation='relu'),  # Primera capa oculta con 16 neuronas
+    tf.keras.layers.Dense(units=32, activation='relu'),  # Segunda capa oculta con 32 neuronas
+    tf.keras.layers.Dense(units=16, activation='relu'),  # Tercera capa oculta con 16 neuronas
+    tf.keras.layers.Dense(units=9, activation='softmax')  # Capa de salida con 9 neuronas (una por cada clase)
+])
+
+# Compilar el modelo
+modelo.compile(
+    optimizer=tf.keras.optimizers.Adam(0.001),
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+print('Inicio de entrenamiento...')
+historial = modelo.fit(features, targets, epochs=500, batch_size=5, validation_split=0.2, verbose=True)
+print('Modelo entrenado!')
+
+# Visualizar la pérdida y la precisión durante el entrenamiento
+plt.figure(figsize=(12, 4))
+plt.subplot(1, 2, 1)
+plt.xlabel('#Época')
+plt.ylabel('Pérdida')
+plt.plot(historial.history['loss'], label='Pérdida de entrenamiento')
+plt.plot(historial.history['val_loss'], label='Pérdida de validación')
+plt.legend()
+
+plt.subplot(1, 2, 2)
+plt.xlabel('#Época')
+plt.ylabel('Precisión')
+plt.plot(historial.history['accuracy'], label='Precisión de entrenamiento')
+plt.plot(historial.history['val_accuracy'], label='Precisión de validación')
+plt.legend()
+
+plt.show()
+
+
+modelo.save('phylum_model_mejorado.h5')
